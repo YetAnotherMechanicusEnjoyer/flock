ff5f78225a8532944eaeaa9c95fd39f249a4134b
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-07-16 23:18:51 +0200

    feat: add core functionnalities

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+mod core;
+mod simulation;
+mod terminal;
+#[allow(unused)]
+mod utils;
+
+use bevy::{prelude::*, window::WindowResolution};
+use core::state::AppState;
+
+pub fn run() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Flock - Terminal".into(),
+                resolution: WindowResolution::new(1280, 720),
+                present_mode: bevy::window::PresentMode::AutoNoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .init_state::<AppState>()
+        .add_plugins((simulation::SimulationPlugin, terminal::TerminalPlugin))
+        .add_systems(Startup, |mut next_state: ResMut<NextState<AppState>>| {
+            next_state.set(AppState::ActiveSimulation);
+        })
+        .run();
+}
